87121dc8ef8eb36c119aeb5d0bdbf341c9d73bad
qianghaohao <codenutter@qq.com>
2016-08-15 01:12:07 +0800

    Update ttk.py

--- src/ttk.py
+++ src/ttk.py
@@ -5,8 +5,9 @@
 # 上做了一些改进:
 #            1.从py3.x改成了py2.x
 #            2.*可以支持票价查询*
-#	         3.使用colorama模块在终端显示彩色字符,可以很好地兼容windows
+#            3.使用colorama模块在终端显示彩色字符,可以很好地兼容windows
 #            4.程序中编码统一为GB2312,很好地支持中文
+#
 # Filename: ttk.py
 # Author: qianghaohao(CodeNutter)
 # Mail: codenutter@foxmail.com
